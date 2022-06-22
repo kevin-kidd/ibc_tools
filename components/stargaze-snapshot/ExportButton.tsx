9c0d320f5ea17699ae5cdef0b7c358004b3e0d5c
Kevin Harrison <62897443+kh-codes@users.noreply.github.com>
2022-06-22 05:13:14 -0400

    Updated ExportButton style.

--- components/stargaze-snapshot/ExportButton.tsx
+++ components/stargaze-snapshot/ExportButton.tsx
@@ -44,7 +44,12 @@ const ExportButton: FunctionComponent<StateProps> = (props) => {
                 target='_blank'
             />
             <div>
-                <Menu.Button className="inline-flex justify-center w-full border border-gray-300 shadow-sm px-4 py-2 bg-[#feef89] text-sm font-medium text-gray-700 focus:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-[#85ff89]">
+                <Menu.Button className="inline-flex justify-center w-full focus:outline-none
+                font-extrabold inline-flex
+                         border-[#1d1d1d] border yellow-button transition duration-200
+                         text-md bg-[#fff985] px-4 py-1 mr-2 focus:outline-none
+
+                ">
                     <ChevronDownIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                     Export
                 </Menu.Button>
@@ -59,7 +64,7 @@ const ExportButton: FunctionComponent<StateProps> = (props) => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="origin-top-right absolute right-0 mt-1 w-full shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+                <Menu.Items className="origin-top-right absolute right-0 mt-1 w-full shadow-lg bg-white ring-1 border border-black ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
                     <div className="py-1">
                         <Menu.Item>
                             <a
