864c95044b4d473c4c560b0d6d03842093267b24
Kevin Harrison <62897443+kh-codes@users.noreply.github.com>
2022-08-09 15:14:25 -0400

    Added redirect for old snapshot slug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/stargaze-snapshot',
+        destination: '/stargaze/snapshot',
+        permanent: true,
+      }
+    ]
+  }
+}
